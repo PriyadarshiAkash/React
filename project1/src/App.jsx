f9558baa5d2e3f2ccff79d0f9f53cd97f0f16fad
akash <aakashpriyadarshi2002@gmail.com>
2024-02-21 18:21:32 +0530

    react

---
+++ project1/src/App.jsx
@@ -0,0 +1,21 @@
+// import logo from './logo.svg';
+import './App.css';
+import HeroSection from './components/Hero';
+import Navigation from './components/Navigation';
+
+function App() {
+  return (
+    <div className="App">
+
+      <div>
+
+        <Navigation/>
+        <HeroSection/>
+        
+      </div>
+    
+    </div>
+  );
+}
+
+export default App;
